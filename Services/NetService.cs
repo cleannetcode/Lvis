d03d10e25416729b730d4ef1bbbbec617675f0c2
Danyello <ulkiora.panda@gmail.com>
2021-04-03 20:43:13 +0600

    added GET request

--- Services/NetService.cs
+++ Services/NetService.cs
@@ -6,27 +6,55 @@ using System.Net;
 using System.IO;
 using System.Threading.Tasks;
 using System.Threading;
+using Newtonsoft.Json;
 
 namespace YouTubeChatBot.Services
 {
     class NetService
     {
-        public async Task<Response> Request(Uri url, RequestMethod method = RequestMethod.GET, IDictionary<string, string> headers = null, byte[] body = null)
-        {
+        private readonly HttpClient client;
 
-        }
-        public class Response
-        {
-            public readonly byte[] Data;
-            public readonly int StatusCode;
-        }
         public enum RequestMethod
         {
             DELETE,
             GET,
             PATCH,
             POST,
-            PUT
+            PUT,
         }
+
+        public async Task<Response> Request(Uri url, RequestMethod method = RequestMethod.GET, IDictionary<string, string> headers = null, byte[] body = null)
+        {
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                {
+                    switch (header.Key.ToString())
+                    {
+                        case "DELETE":
+
+                            break;
+                        case "GET":
+                            var res = await client.GetAsync(header.Key);
+                            Response response = new Response(body, (int)res.StatusCode);
+                            return response;
+                        case "PATCH":
+
+                            break;
+                        case "POST":
+
+                            break;
+                        case "PUT":
+
+                            break;
+                        default:
+                            //throw new HttpRequestException("Invalid Request");
+                            break;
+                    }
+                }
+            }
+        }
+
+
     }
 }
---
+++ Services/Response.cs
@@ -0,0 +1,13 @@
+﻿namespace YouTubeChatBot.Services
+{
+    public class Response
+    {
+        public readonly byte[] Data;
+        public readonly int StatusCode;
+        public Response(byte[] data,int statusCode)
+        {
+            Data = data;
+            StatusCode = statusCode;
+        }
+    }
+}
