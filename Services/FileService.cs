297398f0a26fb1528cdecc11f1dfb900073e9f5f
Vladimir <dementev438@gmail.com>
2021-04-03 22:27:28 +0700

    Revert "1"
    This reverts commit d5a834dc5e696c60d3b91c2782995130085faf77.

--- Services/FileService.cs
+++ Services/FileService.cs
@@ -9,12 +9,27 @@ namespace YouTubeChatBot.Services
     {
         public void Write(string source, string path)
         {
-            File.WriteAllText(source, path);
+            using (FileStream fstream = new FileStream($"{path}\note.txt", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(source);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
         }
-
         public string Read(string path)
         {
-            return File.ReadAllText(path);
+            using (FileStream fstream = File.OpenRead($"{path}\note.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                return textFromFile;
+            }
         }
     }
 }
