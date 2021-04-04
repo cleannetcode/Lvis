9fa2420817918591ad70e6353c1c90fc754c1cbb
Jake Epps <Molniz-95@yandex.ru>
2021-04-04 13:04:48 +0300

    module test update

---
+++ Models/SaveModuleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeChatBot.Models
+{
+    class SaveModuleConfiguration
+    {
+        public string SavePath { get; set; }
+        public string KeyWord { get; set; }
+    }
+}
