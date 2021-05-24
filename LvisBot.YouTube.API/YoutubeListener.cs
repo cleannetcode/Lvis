42f8956ab1588a7c7b20e548b4277bd25042bfc6
Alexander <trulander@mail.ru>
2021-05-24 17:32:11 +0500

    Init youtube api class

---
+++ LvisBot.YouTube.API/YoutubeListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using LvisBot.Domain.Interfaces;
+using LvisBot.Domain.Models;
+
+namespace LvisBot.YouTube.API
+{
+    public class YoutubeListener : ISourceListener<YouTubeConfig, YTMessageResponse, StatusResponse>
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event Action<YTMessageResponse> OnMessageReceived;
+        public event Action<StatusResponse> OnStatusReceived;
+        public void Run(YouTubeConfig configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RunAsync(YouTubeConfig configuration, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
